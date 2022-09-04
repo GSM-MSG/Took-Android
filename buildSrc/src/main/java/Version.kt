6d37ecb56a4f64ca712e572e5252447b78efcf36
김현승 <80810303+khs3994@users.noreply.github.com>
2022-09-05 01:28:35 +0900

    :memo: :: compileSdk Version change

--- buildSrc/src/main/java/Version.kt
+++ buildSrc/src/main/java/Version.kt
@@ -9,7 +9,7 @@ object Versions {
     const val KOTLINX_COROUTINES = "1.6.0"
 
     const val BUILD_GRADLE = "4.2.1"
-    const val COMPILE_SDK_VERSION = 30
+    const val COMPILE_SDK_VERSION = 31
     const val BUILD_TOOLS_VERSION = "32.0.0"
     const val MIN_SDK_VERSION = 29
     const val TARGET_SDK_VERSION = 30
