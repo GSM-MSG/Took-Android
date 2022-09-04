df9995f52f27bc81fd0c5669f00c55bd98ee62b4
이현빈 <82383983+leehyeonbin@users.noreply.github.com>
2022-09-05 01:29:18 +0900

    📄 :: create background screen

--- presentation/src/main/java/com/msg/presentation/MainActivity.kt
+++ presentation/src/main/java/com/msg/presentation/MainActivity.kt
@@ -1,40 +1,49 @@
 package com.msg.presentation
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.blur
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.msg.presentation.ui.theme.TookAndroidTheme
+import com.msg.presentation.ui.theme.TookBackground
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            TookAndroidTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colors.background) {
-                    Greeting("Android")
-                }
-            }
+            TookBackground()
         }
     }
+
+
 }
 
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun buttonClickListener() {
+    Log.d("TAG", "click")
 }
+
 
 @Preview(showBackground = true)
 @Composable
-fun DefaultPreview() {
+fun Preview() {
     TookAndroidTheme {
-        Greeting("Android")
+        TookBackground()
     }
 }
+
