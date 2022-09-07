6abc1330ffc8fb8b704e9ae7fd7c088066680b3d
이현빈 <82383983+leehyeonbin@users.noreply.github.com>
2022-09-07 23:41:30 +0900

    ⚡️ :: 버튼 클릭시 예외처리

--- presentation/src/main/java/com/msg/presentation/ui/nfc/NFCActivity.kt
+++ presentation/src/main/java/com/msg/presentation/ui/nfc/NFCActivity.kt
@@ -1,22 +1,25 @@
 package com.msg.presentation.ui.nfc
 
 import android.app.PendingIntent
+import android.content.Context
 import android.content.Intent
+import android.nfc.FormatException
 import android.nfc.NfcAdapter
 import android.os.Bundle
 import android.util.Log
 import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.Button
 import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import com.msg.presentation.ui.theme.Background
+import java.io.IOException
 
 class NFCActivity : ComponentActivity() {
 
@@ -25,31 +28,52 @@ class NFCActivity : ComponentActivity() {
     private var nfcAdapter: NfcAdapter? = null
     private val NFCTagAvailable = "NFC is not available"
     private val WriteSuccess = "Text Write Successfully"
-    private val WriteError = ""
+    private val WriteError = "Failed to write text"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         nfcAdapter = NfcAdapter.getDefaultAdapter(this)
-        if(nfcAdapter == null) {
+        if (nfcAdapter == null) {
             Log.d(TAG, NFCTagAvailable)
         } else {
             Log.d(TAG, "$nfcAdapter")
         }
         nfcPendingIntent = PendingIntent.getActivity(
             this, 0,
-            Intent(this, javaClass).addFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP), 0)
+            Intent(this, javaClass).addFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP), 0
+        )
         setContent {
             Background()
-            Column(modifier = Modifier) {
+            Column(
+                modifier = Modifier.fillMaxSize(),
+                verticalArrangement = Arrangement.Center,
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
                 Button(
-                    onClick = { }, modifier = Modifier
-                        .fillMaxWidth()
+                    onClick = { writeNFC(context = this@NFCActivity) }, modifier = Modifier
+                        .width(300.dp)
                         .height(50.dp)
                 ) {
                     Text(text = "NFC")
                 }
             }
+        }
+    }
+
+    private fun writeNFC(context: Context) {
+        try {
+            if(nfcAdapter == null) {
+                Toast.makeText(context, NFCTagAvailable, Toast.LENGTH_SHORT).show()
+            } else {
+
+            }
+        } catch (e: IOException) {
+            Toast.makeText(context, WriteError, Toast.LENGTH_SHORT).show()
+            e.printStackTrace()
+        } catch (e: FormatException) {
+            Toast.makeText(context, WriteError, Toast.LENGTH_SHORT).show()
+            e.printStackTrace()
         }
     }
 }
