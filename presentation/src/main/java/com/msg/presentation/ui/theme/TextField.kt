9f3453c5a49ea7a4488c1ed908aef990f8a0e3d8
100Seung-Min <81063667+100Seung-Min@users.noreply.github.com>
2022-09-09 21:19:24 +0900

    ♻️ :: TextField label추가

--- presentation/src/main/java/com/msg/presentation/ui/theme/TextField.kt
+++ presentation/src/main/java/com/msg/presentation/ui/theme/TextField.kt
@@ -2,9 +2,7 @@ package com.msg.presentation.ui.theme
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.BasicTextField
 import androidx.compose.foundation.text.KeyboardActions
@@ -18,44 +16,59 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.focus.FocusRequester
 import androidx.compose.ui.focus.focusRequester
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.text.input.PasswordVisualTransformation
 import androidx.compose.ui.unit.dp
+import com.msg.presentation.R
 
 @Composable
 fun TextFieldNormal(
+    label: Int?,
+    labelTopMargin: Int = 32,
+    topMargin: Int = 4,
     text: String?,
     textChange: (String) -> Unit,
-    placeholder: String,
+    placeholder: Int,
     isError: Boolean,
     onDone: () -> Unit,
     imeAction: ImeAction = ImeAction.Done,
     modifier: Modifier = Modifier
 ) {
-    OutlinedTextField(
-        value = text ?: "",
-        onValueChange = textChange,
-        textStyle = TextStyle(color = Color.White),
-        placeholder = { DefaultText(text = placeholder, fontSize = 16, textColor = Color.Gray) },
-        modifier = modifier
-            .fillMaxWidth()
-            .height(56.dp)
-            .padding(horizontal = 16.dp)
-            .background(Color(0x661A1A20)),
-        singleLine = true,
-        isError = isError,
-        colors = TextFieldDefaults.outlinedTextFieldColors(
-            focusedBorderColor = Color.Blue,
-            errorBorderColor = Color.Red,
-            unfocusedBorderColor = Color(0xFF1D191E)
-        ),
-        shape = RoundedCornerShape(16.dp),
-        keyboardActions = KeyboardActions(
-            onDone = { onDone() }
-        ),
-        keyboardOptions = KeyboardOptions.Default.copy(imeAction = imeAction)
-    )
+    Column {
+        if (label != null) {
+            DefaultText(
+                text = stringResource(id = label),
+                fontSize = 14,
+                modifier = Modifier.padding(start = 16.dp, top = labelTopMargin.dp)
+            )
+        }
+        Spacer(modifier = Modifier.height(topMargin.dp))
+        OutlinedTextField(
+            value = text ?: "",
+            onValueChange = textChange,
+            textStyle = TextStyle(color = Color.White),
+            placeholder = { DefaultText(text = stringResource(id = placeholder), fontSize = 16, textColor = Color.Gray) },
+            modifier = modifier
+                .fillMaxWidth()
+                .height(56.dp)
+                .padding(horizontal = 16.dp)
+                .background(Color(0x661A1A20)),
+            singleLine = true,
+            isError = isError,
+            colors = TextFieldDefaults.outlinedTextFieldColors(
+                focusedBorderColor = Color.Blue,
+                errorBorderColor = Color.Red,
+                unfocusedBorderColor = Color(0xFF1D191E)
+            ),
+            shape = RoundedCornerShape(16.dp),
+            keyboardActions = KeyboardActions(
+                onDone = { onDone() }
+            ),
+            keyboardOptions = KeyboardOptions.Default.copy(imeAction = imeAction)
+        )
+    }
 }
 
 @Composable
