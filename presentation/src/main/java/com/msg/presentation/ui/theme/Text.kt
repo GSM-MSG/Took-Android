e15f26b0bc843eabf475de92d9d5d56cb501e481
100Seung-Min <81063667+100Seung-Min@users.noreply.github.com>
2022-09-09 21:11:30 +0900

    📝 :: ErrorText 추가

--- presentation/src/main/java/com/msg/presentation/ui/theme/Text.kt
+++ presentation/src/main/java/com/msg/presentation/ui/theme/Text.kt
@@ -1,14 +1,22 @@
 package com.msg.presentation.ui.theme
 
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.Icon
 import androidx.compose.material.Text
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ErrorOutline
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.msg.presentation.R
 
@@ -39,4 +47,28 @@ fun DefaultText(text: String, fontSize: Int, textColor: Color = Color.White, mod
         ),
         modifier = modifier
     )
+}
+
+@Composable
+fun ErrorText(isError: Boolean, errorMsg: Int) {
+    if (isError) {
+        Row(
+            modifier = Modifier
+                .padding(start = 16.dp, top = 4.dp)
+                .height(28.dp),
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Icon(
+                imageVector = Icons.Default.ErrorOutline,
+                contentDescription = null,
+                tint = Color.Red
+            )
+            DefaultText(
+                text = stringResource(id = errorMsg),
+                fontSize = 12,
+                textColor = Color.Red,
+                modifier = Modifier.padding(start = 4.dp)
+            )
+        }
+    }
 }
