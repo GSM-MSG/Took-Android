e4347945940a9e71eb63b1660fdd3d2c369e9411
100Seung-Min <81063667+100Seung-Min@users.noreply.github.com>
2022-09-09 22:55:25 +0900

    ⚡️ :: TextField 아이콘 클릭

--- presentation/src/main/java/com/msg/presentation/ui/theme/TextField.kt
+++ presentation/src/main/java/com/msg/presentation/ui/theme/TextField.kt
@@ -1,25 +1,22 @@
 package com.msg.presentation.ui.theme
 
 import androidx.compose.foundation.background
-import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.foundation.text.BasicTextField
 import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.Icon
 import androidx.compose.material.OutlinedTextField
-import androidx.compose.material.Text
-import androidx.compose.material.TextField
 import androidx.compose.material.TextFieldDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.focus.FocusRequester
-import androidx.compose.ui.focus.focusRequester
-import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.text.input.VisualTransformation
 import androidx.compose.ui.unit.dp
 import com.msg.presentation.R
 
@@ -32,46 +29,61 @@ fun TextFieldNormal(
     textChange: (String) -> Unit,
     placeholder: Int,
     isError: Boolean,
-    onDone: () -> Unit,
-    imeAction: ImeAction = ImeAction.Done,
-    modifier: Modifier = Modifier
+    onDone: () -> Unit = {},
+    modifier: Modifier = Modifier,
+    iconClick: () -> Unit,
+    visible: Boolean = true,
+    visibleIcon: Int = R.drawable.ic_cancel,
+    unVisibleIcon: Int = R.drawable.ic_cancel
 ) {
     Column {
         if (label != null) {
             DefaultText(
                 text = stringResource(id = label),
                 fontSize = 14,
-                modifier = Modifier.padding(start = 16.dp, top = labelTopMargin.dp)
+                modifier = Modifier.padding(start = 16.dp, top = labelTopMargin.dp),
+                textColor = White3
             )
         }
         Spacer(modifier = Modifier.height(topMargin.dp))
         OutlinedTextField(
             value = text ?: "",
+            visualTransformation = if(visible) VisualTransformation.None else PasswordVisualTransformation(),
             onValueChange = textChange,
-            textStyle = TextStyle(color = Color.White),
-            placeholder = { DefaultText(text = stringResource(id = placeholder), fontSize = 16, textColor = Color.Gray) },
+            textStyle = TextStyle(color = White2),
+            placeholder = {
+                DefaultText(
+                    text = stringResource(id = placeholder),
+                    fontSize = 16,
+                    textColor = Gray3
+                )
+            },
             modifier = modifier
                 .fillMaxWidth()
                 .height(56.dp)
                 .padding(horizontal = 16.dp)
-                .background(Color(0x661A1A20)),
+                .background(Black3),
             singleLine = true,
             isError = isError,
             colors = TextFieldDefaults.outlinedTextFieldColors(
-                focusedBorderColor = Color.Blue,
-                errorBorderColor = Color.Red,
-                unfocusedBorderColor = Color(0xFF1D191E)
+                focusedBorderColor = Purple2,
+                errorBorderColor = Red,
+                unfocusedBorderColor = Black1
             ),
             shape = RoundedCornerShape(16.dp),
             keyboardActions = KeyboardActions(
                 onDone = { onDone() }
             ),
-            keyboardOptions = KeyboardOptions.Default.copy(imeAction = imeAction)
+            trailingIcon = {
+                Icon(
+                    painter = painterResource(id = if(visible) unVisibleIcon else visibleIcon),
+                    contentDescription = null,
+                    tint = if(text.isNullOrEmpty()) Transparent else Gray1,
+                    modifier = Modifier.clickable {
+                        iconClick()
+                    }
+                )
+            }
         )
     }
-}
-
-@Composable
-fun TextFieldVisible() {
-
 }
