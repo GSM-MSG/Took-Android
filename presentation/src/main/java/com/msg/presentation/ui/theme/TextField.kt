778c7ad66dcbb908675839270bcc137cce43d92f
100Seung-Min <81063667+100Seung-Min@users.noreply.github.com>
2022-09-09 20:00:46 +0900

    📝 :: TextField border/ singleLine

--- presentation/src/main/java/com/msg/presentation/ui/theme/TextField.kt
+++ presentation/src/main/java/com/msg/presentation/ui/theme/TextField.kt
@@ -6,31 +6,59 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.BasicTextField
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.OutlinedTextField
 import androidx.compose.material.Text
 import androidx.compose.material.TextField
+import androidx.compose.material.TextFieldDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.focus.FocusRequester
+import androidx.compose.ui.focus.focusRequester
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.text.input.PasswordVisualTransformation
 import androidx.compose.ui.unit.dp
 
 @Composable
 fun TextFieldNormal(
-    text: String,
+    text: String?,
     textChange: (String) -> Unit,
-    placeholder: String
+    placeholder: String,
+    isError: Boolean,
+    onDone: () -> Unit,
+    imeAction: ImeAction = ImeAction.Done,
+    modifier: Modifier = Modifier
 ) {
     OutlinedTextField(
         value = text ?: "",
         onValueChange = textChange,
         textStyle = TextStyle(color = Color.White),
-        placeholder = { Text(text = placeholder, color = Color(0xFF7E7E87))},
-        modifier = Modifier
+        placeholder = { DefaultText(text = placeholder, fontSize = 16, textColor = Color.Gray) },
+        modifier = modifier
             .fillMaxWidth()
             .height(56.dp)
             .padding(horizontal = 16.dp)
-            .background(Color(0x661A1A20))
-            .border(color = Color(0xFF1D191E), width = 1.dp, shape = RoundedCornerShape(16.dp)),
+            .background(Color(0x661A1A20)),
+        singleLine = true,
+        isError = isError,
+        colors = TextFieldDefaults.outlinedTextFieldColors(
+            focusedBorderColor = Color.Blue,
+            errorBorderColor = Color.Red,
+            unfocusedBorderColor = Color(0xFF1D191E)
+        ),
+        shape = RoundedCornerShape(16.dp),
+        keyboardActions = KeyboardActions(
+            onDone = { onDone() }
+        ),
+        keyboardOptions = KeyboardOptions.Default.copy(imeAction = imeAction)
     )
+}
+
+@Composable
+fun TextFieldVisible() {
+
 }
