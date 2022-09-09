787153dc2ed2267afba983ff1e885ae422c37466
100Seung-Min <81063667+100Seung-Min@users.noreply.github.com>
2022-09-09 22:55:43 +0900

    ⚙️ :: Color 세팅

--- presentation/src/main/java/com/msg/presentation/ui/theme/Color.kt
+++ presentation/src/main/java/com/msg/presentation/ui/theme/Color.kt
@@ -7,12 +7,29 @@ val Purple500 = Color(0xFF6200EE)
 val Purple700 = Color(0xFF3700B3)
 val Teal200 = Color(0xFF03DAC5)
 
-val BtnGradientPurple = listOf(
+val GradientPurple = listOf(
     Color(0xFF6841E8),
     Color(0xFF8D4FE7)
 )
 
-val BtnGradientGray = listOf(
+val GradientGray = listOf(
     Color(0xFF5F5F5F),
     Color(0xFF727272)
 )
+
+val White1 = Color(0xFFFFFFFF)
+val White2 = Color(0xFFEEEEEE)
+val White3 = Color(0xFFD9D9D9)
+val Red = Color(0xFFFF6060)
+val Purple1 = Color(0xFF8D4FE7)
+val Purple2 = Color(0xFF6841E8)
+val Gray1 = Color(0xFFBDBDBD)
+val Gray2 = Color(0xFF969696)
+val Gray3 = Color(0xFF7E7E87)
+val Gray4 = Color(0xFF727272)
+val Gray5 = Color(0xFF5B5B5B)
+val Gray6 = Color(0xFF5F5F5F)
+val Black1 = Color(0xFF1D191E)
+val Black2 = Color(0xFF000000)
+val Black3 = Color(0x661A1A20)
+val Transparent = Color.Transparent
