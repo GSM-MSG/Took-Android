c80586602363ea1ff6932c729e406172c1e150c9
100Seung-Min <81063667+100Seung-Min@users.noreply.github.com>
2022-09-10 11:29:35 +0900

    ⚡️ :: 이용약관

--- presentation/src/main/java/com/msg/presentation/ui/signup/SignUpScreen.kt
+++ presentation/src/main/java/com/msg/presentation/ui/signup/SignUpScreen.kt
@@ -1,38 +1,60 @@
 package com.msg.presentation.ui.signup
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalSoftwareKeyboardController
-import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.unit.dp
 import com.msg.presentation.R
 import com.msg.presentation.ui.theme.*
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.launch
 
+
+@OptIn(ExperimentalMaterialApi::class)
 @Composable
 fun SignUpScreen(back: () -> Unit, toLogin: () -> Unit) {
+    val sheetState = rememberModalBottomSheetState(
+        initialValue = ModalBottomSheetValue.Hidden,
+        skipHalfExpanded = true
+    )
+    val scope = rememberCoroutineScope()
     Background()
-    Column {
-        TookAppBar(back = { back() }, title = R.string.registration)
-        signUpField(toLogin = { toLogin() })
+    ModalBottomSheetLayout(
+        sheetState = sheetState,
+        sheetBackgroundColor = White1,
+        sheetShape = RoundedCornerShape(topStart = 32.dp, topEnd = 32.dp),
+        sheetContent = {
+            bottomTerms()
+        }) {
+        Column {
+            TookAppBar(back = { back() }, title = R.string.registration)
+            signUpField(toLogin = { toLogin() }, scope = scope, state = sheetState)
+        }
     }
 }
 
-@OptIn(ExperimentalComposeUiApi::class)
+@OptIn(ExperimentalComposeUiApi::class, ExperimentalMaterialApi::class)
 @Composable
-fun signUpField(toLogin: () -> Unit) {
+fun signUpField(toLogin: () -> Unit, scope: CoroutineScope, state: ModalBottomSheetState) {
     var email by remember { mutableStateOf<String?>(null) }
     var password by remember { mutableStateOf<String?>(null) }
     var visiblePassword by remember { mutableStateOf<Boolean>(false) }
     var isError by remember { mutableStateOf<Boolean>(false) }
     val keyboardController = LocalSoftwareKeyboardController.current
 
-    fun isLogin() {
+    fun isSignUp() {
         keyboardController?.hide()
         isError = true
+        scope.launch {
+            state.show()
+        }
     }
 
     Column {
@@ -61,7 +83,7 @@ fun signUpField(toLogin: () -> Unit) {
         ErrorText(isError = isError, errorMsg = R.string.wrong_login)
         Spacer(modifier = Modifier.weight(1f))
         signUpBtn(
-            isLogin = { isLogin() },
+            isSignUp = { isSignUp() },
             email = email,
             password = password,
             toLogin = { toLogin() }
@@ -71,15 +93,15 @@ fun signUpField(toLogin: () -> Unit) {
 
 @Composable
 fun signUpBtn(
-    isLogin: () -> Unit,
+    isSignUp: () -> Unit,
     email: String?,
     password: String?,
     toLogin: () -> Unit
 ) {
     ButtonDisable(
-        onClick = { isLogin() },
-        text = stringResource(id = R.string.registration),
-        enabled = email.isNullOrBlank() || password.isNullOrBlank()
+        onClick = { isSignUp() },
+        text = R.string.registration,
+        enabled = !email.isNullOrBlank() && !password.isNullOrBlank()
     )
     Row(
         horizontalArrangement = Arrangement.Center,
@@ -88,19 +110,68 @@ fun signUpBtn(
             .padding(top = 13.dp, bottom = 17.dp)
     ) {
         DefaultText(
-            text = stringResource(id = R.string.is_account),
+            text = R.string.is_account,
             fontSize = 12,
             textColor = Gray1
         )
         DefaultText(
-            text = stringResource(id = R.string.login),
+            text = R.string.login,
             fontSize = 12,
             textColor = Gray1,
             modifier = Modifier
                 .padding(start = 4.dp)
                 .clickable {
-                toLogin()
-            }
+                    toLogin()
+                }
         )
     }
+}
+
+@Composable
+fun bottomTerms() {
+    var check1 by remember { mutableStateOf(false) }
+    var check2 by remember { mutableStateOf(false) }
+    var check3 by remember { mutableStateOf(false) }
+    var check4 by remember { mutableStateOf(false) }
+    var check5 by remember { mutableStateOf(false) }
+    Column(
+        modifier = Modifier
+            .fillMaxWidth()
+            .height(480.dp)
+    ) {
+        DefaultText(
+            text = R.string.agreed,
+            fontSize = 20,
+            textColor = Black2,
+            modifier = Modifier.padding(start = 32.dp, top = 28.dp)
+        )
+        agreedCheckBox(
+            enabled = check1 && check2 && check3 && check4 && check5, checkChange = {
+                check1 = it; check2 = it; check3 = it; check4 = it; check5 = it
+            }
+        )
+        divider()
+        agreed(enabled = check1, checkChange = { check1 = it }, detailAgreed = {})
+        agreed(enabled = check2, checkChange = { check2 = it }, detailAgreed = {})
+        agreed(enabled = check3, checkChange = { check3 = it }, detailAgreed = {})
+        agreed(enabled = check4, checkChange = { check4 = it }, detailAgreed = {})
+        agreed(enabled = check5, checkChange = { check5 = it }, detailAgreed = {})
+        Spacer(modifier = Modifier.weight(1f))
+        ButtonDisable(
+            onClick = {},
+            text = R.string.okay,
+            enabled = (check1 && check2 && check3 && check4 && check5)
+        )
+    }
+}
+
+@Composable
+fun divider() {
+    Box(
+        modifier = Modifier
+            .padding(horizontal = 32.dp, vertical = 16.dp)
+            .fillMaxWidth()
+            .height(1.dp)
+            .background(White2)
+    )
 }
