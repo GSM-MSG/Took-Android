df8046cb288a88b3b6b34a3e384395d5b08fde33
이현빈 <82383983+leehyeonbin@users.noreply.github.com>
2022-09-21 23:42:41 +0900

    ⚡️ :: write 코드 다시짜기

--- presentation/src/main/java/com/msg/presentation/ui/nfc/NFCActivity.kt
+++ presentation/src/main/java/com/msg/presentation/ui/nfc/NFCActivity.kt
@@ -6,6 +6,7 @@ import android.content.Intent
 import android.content.IntentFilter
 import android.nfc.*
 import android.nfc.tech.Ndef
+import android.nfc.tech.NfcF
 import android.os.Bundle
 import android.util.Log
 import android.widget.Toast
@@ -19,8 +20,11 @@ import java.io.IOException
 class NFCActivity : ComponentActivity() {
 
     private var intentFiltersArray: Array<IntentFilter>? = null
-    private lateinit var nfcPendingIntent: PendingIntent
-    private var nfcAdapter: NfcAdapter? = null
+    private val techListsArray = arrayOf(arrayOf(NfcF::class.java.name))
+    private val nfcAdapter: NfcAdapter? by lazy {
+        NfcAdapter.getDefaultAdapter(this)
+    }
+    private var pendingIntent: PendingIntent? = null
     private val TAG = "NFCActivity_TAG"
     private var isRunNFC = false
 
@@ -28,14 +32,10 @@ class NFCActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
 
         setContent {
-            NFCScreen(onClick = { checkNFC(this); isRunNFC = !isRunNFC }, nfcAdapter = nfcAdapter)
+            NFCScreen(onClick = { checkNFC(this) }, nfcAdapter = nfcAdapter)
         }
-
-        this.nfcAdapter = NfcAdapter.getDefaultAdapter(this)
-        nfcPendingIntent = PendingIntent.getActivity(
-            this, 0,
-            Intent(this, javaClass),
-            PendingIntent.FLAG_IMMUTABLE
+        pendingIntent = PendingIntent.getActivity(
+            this, 0, Intent(this, javaClass).addFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP), 0
         )
         val ndef = IntentFilter(NfcAdapter.ACTION_NDEF_DISCOVERED)
         try {
@@ -52,60 +52,77 @@ class NFCActivity : ComponentActivity() {
 
     override fun onResume() {
         super.onResume()
-        nfcAdapter?.enableForegroundDispatch(this@NFCActivity, nfcPendingIntent, null, null)
+        nfcAdapter?.enableForegroundDispatch(this, pendingIntent, intentFiltersArray, techListsArray)
     }
 
     override fun onPause() {
         super.onPause()
-        nfcAdapter?.disableForegroundDispatch(this@NFCActivity)
+        if(this.isFinishing) nfcAdapter?.disableForegroundDispatch(this@NFCActivity)
     }
 
-    override fun onNewIntent(intent: Intent?) {
+    override fun onNewIntent(intent: Intent) {
         super.onNewIntent(intent)
-        setIntent(intent)
-        Log.d(TAG, "onNewIntent")
-
-        val detectedTag: Tag? = intent?.getParcelableExtra(NfcAdapter.EXTRA_TAG)
-
-        val writeValue: String = "test"
-        val message: NdefMessage = createTagMessage(writeValue)
-
-        if (detectedTag != null && isRunNFC) writeTag(message, detectedTag)
-    }
-
-    private fun createTagMessage(msg: String): NdefMessage {
-        return NdefMessage(NdefRecord.createTextRecord("en", msg))
-    }
-
-    private fun writeTag(message: NdefMessage, tag: Tag) {
-        val size = message.toByteArray().size
         try {
-            val ndef = Ndef.get(tag)
-            if (ndef != null) {
-                ndef.connect()
-                Toast.makeText(applicationContext, "NFC connect", Toast.LENGTH_SHORT).show()
-                if (!ndef.isWritable) {
-                    Toast.makeText(applicationContext, "can not write NFC tag", Toast.LENGTH_SHORT)
-                        .show()
+            val id = "good"
+
+            if (NfcAdapter.ACTION_TECH_DISCOVERED == intent.action
+                || NfcAdapter.ACTION_NDEF_DISCOVERED == intent.action
+            ) {
+                val tag = intent.getParcelableExtra<Tag>(NfcAdapter.EXTRA_TAG) ?: return
+                val ndef = Ndef.get(tag) ?: return
+
+                if (ndef.isWritable) {
+                    var message = NdefMessage(arrayOf(NdefRecord.createTextRecord("en", id)))
+
+                    ndef.connect()
+                    ndef.writeNdefMessage(message)
+                    ndef.close()
+
+                    Toast.makeText(applicationContext, "Successfully Wroted!", Toast.LENGTH_SHORT).show()
                 }
-                if (ndef.maxSize < size) {
-                    Toast.makeText(applicationContext, "NFC tag size too large", Toast.LENGTH_SHORT)
-                        .show()
-                }
-                ndef.writeNdefMessage(message)
-                Toast.makeText(applicationContext, "NFC tag is writted", Toast.LENGTH_SHORT).show()
-            } else Toast.makeText(applicationContext, "NFC connect failed", Toast.LENGTH_SHORT)
-                .show()
+            } else {
+                Toast.makeText(applicationContext, "Write on text box!", Toast.LENGTH_SHORT).show()
+            }
         } catch (e: Exception) {
-            Log.i(TAG, e.message!!)
-            Toast.makeText(applicationContext, "Exception", Toast.LENGTH_SHORT).show()
-            e.printStackTrace()
+            e.message
         }
+        Log.d(TAG, "onNewIntent")
     }
+
+//    private fun createTagMessage(msg: String): NdefMessage {
+//        return NdefMessage(NdefRecord.createTextRecord("en", msg))
+//    }
+//
+//    private fun writeTag(message: NdefMessage, tag: Tag) {
+//        val size = message.toByteArray().size
+//        try {
+//            val ndef = Ndef.get(tag)
+//            if (ndef != null) {
+//                ndef.connect()
+//                Toast.makeText(applicationContext, "NFC connect", Toast.LENGTH_SHORT).show()
+//                if (!ndef.isWritable) {
+//                    Toast.makeText(applicationContext, "can not write NFC tag", Toast.LENGTH_SHORT)
+//                        .show()
+//                }
+//                if (ndef.maxSize < size) {
+//                    Toast.makeText(applicationContext, "NFC tag size too large", Toast.LENGTH_SHORT)
+//                        .show()
+//                }
+//                ndef.writeNdefMessage(message)
+//                Toast.makeText(applicationContext, "NFC tag is writted", Toast.LENGTH_SHORT).show()
+//            } else Toast.makeText(applicationContext, "NFC connect failed", Toast.LENGTH_SHORT)
+//                .show()
+//        } catch (e: Exception) {
+//            Log.i(TAG, e.message!!)
+//            Toast.makeText(applicationContext, "Exception", Toast.LENGTH_SHORT).show()
+//            e.printStackTrace()
+//        }
+//    }
 
 
     //    버튼을 클릭하였을 때 쓰기 모드로 변환하기
     private fun checkNFC(context: Context) {
+        isRunNFC = !isRunNFC
         try {
             if (nfcAdapter == null) {
                 Toast.makeText(context, "nfcTagAvailable", Toast.LENGTH_SHORT).show()
